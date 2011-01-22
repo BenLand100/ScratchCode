25f9e89b527f8407ed39280258ee67b1d88c7063
Benjamin Land <benland100@gmail.com>
2011-01-22 10:21:12 -0500

    Starting on the Granular Material Simulator I've been meaning to write

---
+++ GranularSim/main.cpp
@@ -0,0 +1,29 @@
+/**
+ *  Copyright 2011 by Benjamin J. Land (a.k.a. BenLand100)
+ *
+ *  This file is part of GranularSim, a Granular Material Simulator.
+ *
+ *  GranularSim is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GranularSim is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GranularSim. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ GranularSim/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=granularsim
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/granularsim
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=granularsim.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/granularsim.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=granularsim
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/granularsim
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=granularsim.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/granularsim.tar
